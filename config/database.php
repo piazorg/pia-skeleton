e339923878e4af4b1651f2f293df0f3b159c03d7
Eddie Palmans <eddie@palmans.nl>
2024-06-27 14:51:40 +0200

    PDO::SQLSRV_ATTR_FETCHES_NUMERIC_TYPE for (PDO) sqlsrv driver

--- config/database.php
+++ config/database.php
@@ -91,6 +91,9 @@ return [
             'prefix_indexes' => true,
             // 'encrypt' => env('DB_ENCRYPT', 'yes'),
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
+            'options' => extension_loaded("pdo_sqlsrv") ? [
+                PDO::SQLSRV_ATTR_FETCHES_NUMERIC_TYPE => true,
+            ] : [],
         ],
 
     ],
